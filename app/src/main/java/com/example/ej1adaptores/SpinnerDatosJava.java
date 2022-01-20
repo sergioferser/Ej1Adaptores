03b75a9ee822f169a5f95a695a55233d88542ee1
a20martincm <martinmcm@hotmail.es>
2022-01-20 10:06:16 +0100

    Primer commit

--- app/src/main/java/com/example/ej1adaptores/SpinnerDatosJava.java
+++ app/src/main/java/com/example/ej1adaptores/SpinnerDatosJava.java
@@ -3,9 +3,12 @@ package com.example.ej1adaptores;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.Toast;
 
 public class SpinnerDatosJava extends AppCompatActivity {
 
@@ -19,11 +22,33 @@ public class SpinnerDatosJava extends AppCompatActivity {
 
         spinner = findViewById(R.id.spPlanetasDatosJava);
 
-        String[] arrayPlanetas = {"Mercurio", "Venus","Terra", "Marte", "Júpiter","Saturno","Urano","Neptuno", "Pluton" };
+        String[] arrayPlanetas = {"Mercurio", "Venus","Tierra", "Marte", "Júpiter","Saturno","Urano","Neptuno", "Pluton" };
 
         // creamos el adaptador(simple)
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SpinnerDatosJava.this, android.R.layout.simple_spinner_item, arrayPlanetas);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SpinnerDatosJava.this, android.R.layout.simple_spinner_dropdown_item, arrayPlanetas);
+       //asignanos el adaptador a la vista spinner en donde se mostrará
+        //dichos datos
         spinner.setAdapter(adapter);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //obtener el elemnto seleccionado
+                String seleccion1 = spinner.getSelectedItem().toString();
+                //otra forma de obtener el item seleccionado
+                String seleccion2 = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(SpinnerDatosJava.this,"Estamos en "+ spinner.getSelectedItem().toString()+ "\nseleccion "+ seleccion2,Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
 
 
 
